18d5a3dc2a250dfced48c15a2a00e14880218fe7
SonniePeris <sonnieperis17@gmail.com>
2026-02-10 14:28:56 -0700

    initial commit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -31,3 +31,12 @@ const director1: Directors = {
 };
 
 console.log(director1);
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe'));
